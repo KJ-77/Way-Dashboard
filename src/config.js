90f0e3616bdd67c123bdb3fcda5eb1f8a1b0bacf
Khalil <kjamil2004@gmail.com>
2025-11-29 05:51:57 +0200

    Update API URLs to production endpoints
    Changed BACKEND_URL and BASE_URL from localhost to the production API at api.waybeirut.com for deployment.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const BACKEND_URL = 'http://localhost:5001/api'
+const BACKEND_URL = 'http://api.waybeirut.com/api'
 // const BACKEND_URL = 'https://api.zenith-eng.site/api'
 
 export default BACKEND_URL
-export const BASE_URL = 'http://localhost:5001'
+export const BASE_URL = 'http://api.waybeirut.com'
 // export const BASE_URL = 'https://api.zenith-eng.site'
